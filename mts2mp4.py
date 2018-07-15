b547c91308ec4458e8ef10b6eb78b09f3b5b542e
Oliver Meyer <oliver.meyer@maihiro.com>
2018-07-15 23:40:02 +0200

    almost finished

--- mts2mp4.py
+++ mts2mp4.py
@@ -1,10 +1,30 @@
-from subprocess import Popen
 from sys import stdin
-from os import stat
+from os import path
+from os import walk
+from os import remove
+from subprocess import Popen
+from subprocess import PIPE
+from string import upper
 
-for path in stdin.readlines():
-    stat()
-    wrapAsMP4(path)
 
 def wrapAsMP4(mtsfile):
-    proc = Popen(["ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy", "-f", "mp4", ])
+    mp4file, mts = path.splitext(mtsfile)
+    proc = Popen(["ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy", mp4file + '.mp4'])
+    so, se = proc.communicate(PIPE)
+    for i in so.readlines():
+        print i
+    if proc.returncode == 0:
+        remove(mtsfile)
+
+
+for f in stdin.readlines():
+    if path.isdir(f):
+        if path.basename(f) == 'AVCHD':
+            f = path.join(f, "BDMV", "STREAM")
+            for top, dirs, files in walk(f):
+                for g in files:
+                    b, extension = path.splitext(g)
+                    if upper(e) == '.MTS':
+                        wrapAsMP4(path.join(top, g))
+    else:
+        wrapAsMP4(f)
