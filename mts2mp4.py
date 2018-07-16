5a3053ebcb018c541712cd9814f35a27944ac7b9
zudljk <zudljk@email.de>
2018-07-16 11:39:17 +0200

    add metadata to video file

--- mts2mp4.py
+++ mts2mp4.py
@@ -2,14 +2,18 @@ from sys import stdin
 from os import path
 from os import walk
 from os import remove
+from os import environ
 from subprocess import Popen
 from subprocess import PIPE
 from string import upper
 
 
-def wrapAsMP4(mtsfile):
+def wrap_as_mp4(mtsfile, metadata={}):
     mp4file, mts = path.splitext(mtsfile)
-    proc = Popen(["ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy", mp4file + '.mp4'])
+    params = ["ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy", mp4file + '.mp4']
+    for key, value in metadata:
+
+    proc = Popen(params)
     so, se = proc.communicate(PIPE)
     for i in so.readlines():
         print i
@@ -17,14 +21,24 @@ def wrapAsMP4(mtsfile):
         remove(mtsfile)
 
 
+def get_user_full_name():
+    proc = Popen(['finger', environ["USER"]])
+    sti, ste = proc.communicate(PIPE)
+    for i in so.readlines():
+
+
+
 for f in stdin.readlines():
     if path.isdir(f):
         if path.basename(f) == 'AVCHD':
+            dirmetadata = {
+                "title": path.basename(path.dirname(f)),
+                "author": environ['USER']}
             f = path.join(f, "BDMV", "STREAM")
             for top, dirs, files in walk(f):
                 for g in files:
                     b, extension = path.splitext(g)
                     if upper(extension) == '.MTS':
-                        wrapAsMP4(path.join(top, g))
+                        wrap_as_mp4(path.join(top, g))
     else:
-        wrapAsMP4(f)
+        wrap_as_mp4(f)
