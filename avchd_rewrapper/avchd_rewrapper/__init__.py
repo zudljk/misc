e7cc8777176e0d62a55f04072d3ccadb41f10ee3
zudljk <zudljk@email.de>
2018-07-17 20:48:02 +0200

    Made mts2mp4.py into a package, added Automator workflow

--- avchd_rewrapper/avchd_rewrapper/__init__.py
+++ avchd_rewrapper/avchd_rewrapper/__init__.py
@@ -1,24 +1,27 @@
-from sys import stdin
 from os import path, walk, remove, environ, stat
 from subprocess import Popen, PIPE
 from string import upper, split
 from time import localtime
 
 
-def wrap_as_mp4(mtsfile, metadata):
+def wrap_as_mp4(mtsfile, metadata, outputdir=None):
+    if outputdir is None:
+        outputdir = path.dirname(mtsfile)
+
     mp4file, mts = path.splitext(mtsfile)
+    mp4file = path.join(outputdir, path.basename(mp4file))+'mp4'
+
     params = ["ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy"]
     for key, value in metadata:
         params.append("-metadata")
         params.append(key+'="'+value+'"')
-    params.append(mp4file + '.mp4')
+    params.append(mp4file)
 
     proc = Popen(params)
     so, se = proc.communicate(PIPE)
     for i in so.splitlines():
         print i
-    if proc.returncode == 0:
-        remove(mtsfile)
+    proc.wait()
 
 
 def get_user_full_name(loginname):
@@ -31,12 +34,16 @@ def get_user_full_name(loginname):
 
 
 def wrap_avchd_dir(avchd_dir):
+    user = get_user_full_name(environ["USER"])
+    outdir = path.join(path.dirname(avchd_dir), "mp4")
     for top, dirs, files in walk(path.join(avchd_dir, "BDMV", "STREAM")):
         for g in files:
-            wrap_mts_file(g)
+            wrap_mts_file(g, outdir, user)
 
 
-def wrap_mts_file(mts_file):
+def wrap_mts_file(mts_file, outputdir=None, user=None):
+    if user is None:
+        user = get_user_full_name(environ["USER"])
     track, extension = path.splitext(path.basename(mts_file))
     if upper(extension) == '.MTS':
         try:
@@ -45,17 +52,15 @@ def wrap_mts_file(mts_file):
             pass
         wrap_as_mp4(mts_file, {
             "title": path.basename(path.dirname(mts_file)),
-            "author": u,
+            "author": user,
             "year": localtime(stat(mts_file).st_mtime).tm_year,
             "track": track
-        })
+        }, outputdir)
 
 
-u = get_user_full_name(environ["USER"])
-
-for f in stdin.readlines():
-    if path.isdir(f):
-        if path.basename(f) == 'AVCHD':
-            wrap_avchd_dir(f)
+def wrap_to_mp4(file_or_dir):
+    if path.isdir(file_or_dir):
+        if path.basename(file_or_dir) == 'AVCHD':
+            wrap_avchd_dir(file_or_dir)
     else:
-        wrap_mts_file(f)
+        wrap_mts_file(file_or_dir)
---
+++ avchd_rewrapper/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='avchd_rewrapper',
+      version='0.2',
+      description='Rewrap AVCHD dirs or single MTS files to MP4',
+      url='http://github.com/zudljk/misc/avchd_rewrapper',
+      author='zudljk',
+      author_email='zudljk@email.de',
+      license='LGPL',
+      packages=['avchd_rewrapper'],
+      zip_safe=False)
