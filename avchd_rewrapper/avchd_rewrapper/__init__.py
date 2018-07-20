8f9bc817975e4d813cc3d136fa0c7e5cf57ef1ea
zudljk <zudljk@email.de>
2018-07-20 18:13:02 +0200

    Fixed way in which username is determined

--- avchd_rewrapper/avchd_rewrapper/__init__.py
+++ avchd_rewrapper/avchd_rewrapper/__init__.py
@@ -1,24 +1,32 @@
-from os import path, walk, environ, stat, mkdir, remove
+from os import path, walk, stat, mkdir, remove
 from subprocess import Popen, PIPE
-from string import upper, strip
+from string import upper
 from time import localtime
+from pwd import getpwuid
 import logging
 
 
-def wrap_as_mp4(mtsfile, metadata, outputdir=None):
-    if outputdir is None:
-        outputdir = path.dirname(mtsfile)
+def wrap_as_mp4(mts_file, metadata, output_dir=None):
+    if output_dir is None:
+        output_dir = path.dirname(mts_file)
 
-    if not path.isdir(outputdir):
-        mkdir(outputdir)
+    if not path.isdir(output_dir):
+        mkdir(output_dir)
 
-    mp4file, mts = path.splitext(mtsfile)
-    mp4file = path.join(outputdir, path.basename(mp4file))+'.mp4'
+    mp4file, mts = path.splitext(mts_file)
+    mp4file = path.join(output_dir, path.basename(mp4file)) + '.mp4'
 
-    params = ["/usr/local/bin/ffmpeg", "-i", mtsfile, "-vcodec", "copy", "-acodec", "copy", "-n", "-nostats", "-loglevel", "error"]
+    params = ["/usr/local/bin/ffmpeg",
+              "-i", mts_file,
+              "-vcodec", "copy",
+              "-acodec", "copy",
+              "-n",
+              "-nostats",
+              "-loglevel", "error"
+              ]
     for key, value in metadata.iteritems():
         params.append("-metadata")
-        params.append(key+'='+str(value))
+        params.append(key + '=' + str(value))
     params.append(mp4file)
 
     try:
@@ -29,17 +37,12 @@ def wrap_as_mp4(mtsfile, metadata, outputdir=None):
         raise RuntimeError('Call to ffmpeg failed: %s' % str(e))
 
 
-def get_user_full_name(loginname):
-    so, se = Popen(args=['finger', '-s', loginname], stdout=PIPE).communicate()
-    fullname = loginname
-    for line in so.split('\n'):
-        if len(line) > 0:
-            logon, fullname, rest = line.split('  ', 2)
-    return strip(fullname)
+def get_user_full_name(uid):
+    return getpwuid(uid).pw_gecos
 
 
 def wrap_avchd_dir(avchd_dir):
-    user = get_user_full_name(environ["USER"])
+    user = get_user_full_name(stat(avchd_dir).st_uid)
     outdir = path.join(path.dirname(avchd_dir), "mp4")
     title = path.basename(path.dirname(avchd_dir))
     for top, dirs, files in walk(path.join(avchd_dir, "BDMV", "STREAM")):
@@ -49,11 +52,11 @@ def wrap_avchd_dir(avchd_dir):
 
 def wrap_mts_file(mts_file, outputdir=None, user=None, title=None):
     if user is None:
-        user = get_user_full_name(environ["USER"])
+        user = get_user_full_name(stat(mts_file).st_uid)
     if title is None:
         title = path.basename(path.dirname(mts_file))
     if not path.exists(mts_file):
-        raise OSError('{} not found'.format(mts_file))
+        raise OSError('%s not found' % mts_file)
     track, extension = path.splitext(path.basename(mts_file))
     if upper(extension) == '.MTS':
         try:
@@ -66,14 +69,13 @@ def wrap_mts_file(mts_file, outputdir=None, user=None, title=None):
             "artist": user,
             "year": localtime(stat(mts_file).st_mtime).tm_year,
             "date": localtime(stat(mts_file).st_mtime).tm_year,
-            "track": track+1
+            "track": track + 1
         }, outputdir)
     else:
         raise RuntimeError('%s is not an MTS file' % mts_file)
 
 
 def wrap_to_mp4(file_or_dir):
-
     logger = logging.getLogger('avchd_rewrapper')
     logfilename = path.join(path.dirname(file_or_dir), 'avchd_rewrapper.log')
     logfile = logging.FileHandler(logfilename)
