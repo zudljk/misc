4b80744624490855278dc8c06c1b1476d16568db
Oliver Meyer <zudljk@email.de>
2020-03-04 20:34:23 +0100

    Add rsync and progress

--- photosync.py
+++ photosync.py
@@ -2,30 +2,61 @@
 from tkinter import *
 from tkinter.ttk import Progressbar
 from tkinter.ttk import Style
+from os import path
+from os import listdir
+from os import popen
+
+rsyncTemplate = "echo /usr/local/bin/rsync --archive --verbose --delete-after --exclude .Doc* --exclude .fs* --exclude .Spot* --exclude .Trash* --exclude .Temp* --exclude Backup"
+sourcePath = path.dirname(path.realpath(__file__))
+targetPath = "/Volumes/SeagateExp/Bilder"
+
+if sourcePath.startswith(targetPath):
+    targetPath = targetPath.replace(old="SeagateExp", new="SeagateUSB3")
+
+years = listdir(sourcePath)
+
 
 class PhotoRsync(Tk):
 
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
-        self.title("Hello world!")
-        self.geometry('400x75')
-        self.lbl = Label(self, text="Hello", anchor="ne", justify=LEFT, borderwidth=2)
+        self.title("Syncing ...")
+
+        self.geometry('400x205')
+        self.lbl = Label(self, text="Hello", anchor="nw", justify=LEFT, borderwidth=2)
         self.lbl.place(x=15, y=10)
         self.lbl.pack()
         style = Style()
         style.configure("black.Horizontal.TProgressbar", background='blue', padx=20, justify=CENTER)
         self.progressbar = Progressbar(self, length=370, mode="determinate", style="black.Horizontal.TProgressbar")
         self.progressbar.place(x=15, y=40)
+        self.progressbar.pack()
+        self.output = Text(self, height=8, width=80, bg="seashell3")
+        self.output.pack()
+        self.ok = Button(self, text="Ok", command=self.destroy, state=DISABLED)
+        self.ok.place(x=180, y=60)
+        self.ok.pack()
 
     def start(self):
         self.progressbar['value'] = 0
-        self.progressbar["maximum"] = 40
+        self.progressbar["maximum"] = len(years)
         self.progressbar.pack()
-        self.copy()
+        self.copy(years)
 
-    def copy(self):
+    def copy(self, sources):
+        year = sources.pop(0)
+        self.lbl["text"] = f"{sourcePath}/{year}"
+        with popen(f"{rsyncTemplate} {year} {targetPath}/{year}") as f:
+            line = f.readline()
+            while line:
+                self.output.insert(END, line)
+                line = f.readline()
         self.progressbar['value'] += 1
-        self.after(100, self.copy)
+        if len(sources) > 0:
+            self.after(100, self.copy, sources)
+        else:
+            self.ok["state"] = NORMAL
+
 
 app = PhotoRsync()
 app.start()
--- synctxt.applescript
+++ synctxt.applescript
@@ -4,36 +4,36 @@
 set rsyncTemplate to "/usr/local/bin/rsync --archive --verbose --delete-after --exclude .Doc* --exclude .fs* --exclude .Spot* --exclude .Trash* --exclude .Temp* --exclude Backup "
 
 try
-tell application "Finder"
-	set myDir to (container of (path to me))
-	set targetDir to POSIX file "/Volumes/SeagateExp/Bilder"
-	
-	if myDir is equal to targetDir then
-		set targetDir to POSIX file "/Volumes/SeagateUSB3/Bilder"
-	end if
-	
-	#set progress description to "Bla"
-	# ("Synchronizing " & myDir & " to " & targetDir)
-	
-	set years to folders of myDir
-	set targetDir to POSIX path of targetDir
-end tell
+  tell application "Finder"
+    set myDir to (container of (path to me))
+    set targetDir to POSIX file "/Volumes/SeagateExp/Bilder"
+
+    if myDir is equal to targetDir then
+      set targetDir to POSIX file "/Volumes/SeagateUSB3/Bilder"
+    end if
+
+    #set progress description to "Bla"
+    # ("Synchronizing " & myDir & " to " & targetDir)
+
+    set years to folders of myDir
+    set targetDir to POSIX path of targetDir
+  end tell
 
 set progress description to "Synchronizing ..."
 set progress total steps to the length of years
 
 set i to 1
 repeat with yearDir in years
-	tell application "Finder" to set n to (name of yearDir) as text
-	tell application "Finder" to set f1 to POSIX path of (yearDir as alias)
-	
-	set rsyncCommand to rsyncTemplate & f1 & " " & targetDir & "/" & n
-	set progress additional description to n
-	#			do shell script rsyncCommand
-	delay 1
-	set progress completed steps to i
-	set i to i + 1
+  tell application "Finder" to set n to (name of yearDir) as text
+  tell application "Finder" to set f1 to POSIX path of (yearDir as alias)
+
+  set rsyncCommand to rsyncTemplate & f1 & " " & targetDir & "/" & n
+set progress additional description to n
+#			do shell script rsyncCommand
+delay 1
+set progress completed steps to i
+set i to i + 1
 end repeat
 on error e
-	display alert e
+display alert e
 end try
